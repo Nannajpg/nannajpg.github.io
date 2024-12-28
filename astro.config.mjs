f0f468f19a1f9c9b73af99716cbc294a65841bc7
Nannajpg <61885934+Nannajpg@users.noreply.github.com>
2024-12-27 20:49:32 -0400

    funcionaa

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://nannajpg.github.io',
-  base: 'my-portfolio',
+  base: 'nannajpg.github.io',
 });
